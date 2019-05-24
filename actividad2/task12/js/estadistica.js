acfb4adf2fcfca40e5e7a2c8be0c73233c3f095b
francisco <francisco.javier1988@gmail.com>
2019-05-23 21:55:31 -0300

    commit de la finalizacion de la actividad 5

--- actividad2/task12/js/estadistica.js
+++ actividad2/task12/js/estadistica.js
@@ -14,84 +14,100 @@ rArchivoJson.send();
 function escucharRespuesta(e) {
     json = JSON.parse(this.response);
     var data = json.results[0].members
-    //console.log(data)
+    
     listarPartidos(data)
 }
 
 
 function listarPartidos(arreglo)
 {
-  var valorDemocrata = 0,
-    valorRepublicano = 0,
-    valorIndependiente = 0;
-  var resultadoDemocrata = 0,
-    resultadoRepublicano = 0,
-    resultadoIndependiente;
-  
- 
-
-  democrata = arreglo.filter(arre => arre.party == "D");
-  republican = arreglo.filter(arre => arre.party == "R");
-  independents = arreglo.filter(arre => arre.party == "I");
-  //console.log(democrata)
-
-  for (let i = 0; i < democrata.length; i++) {
-    valorDemocrata += democrata[i].votes_with_party_pct;
-  }
-
-  for (let i = 0; i < republican.length; i++) {
-    valorRepublicano += republican[i].votes_with_party_pct;
-  }
-
-  for (let i = 0; i < independents.length; i++) {
-    valorIndependiente += independents[i].votes_with_party_pct;
-  }
-
-  resultadoDemocrata = (valorDemocrata / democrata.length).toFixed(2);
-  resultadoRepublicano = (valorRepublicano / republican.length).toFixed(2);
-  resultadoIndependiente = valorIndependiente;
-
-  //+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/++/+/+/+/+/+/+/+/+/+/+/
-  //creacion de la tabla 
-  var tabla1 = document.getElementById("estadistica")
-  var mostrar =" "
-     mostrar="<thead class='thead-dark'><tr><th>Party</th><th>Number of Reps</th><th>% Voted with Prty</th></thead>"
-    mostrar += "<tr><td>Republican</td><td>"+republican.length+"</td><td>"+resultadoDemocrata+"</td></tr>"
-    mostrar += "<tr><td>Democrata</td><td>"+democrata.length+"</td><td>"+resultadoRepublicano+"</td></tr>"
-    mostrar += "<tr><td>Independent</td><td>"+independents.length+"</td><td>"+resultadoIndependiente+"</td></tr>"
-
-    tabla1.innerHTML=mostrar  
-  
-
-
-
     var estadisticas = {
-      "numero de democratas": democrata.length,
-      "Promedio de votos Democratas": resultadoDemocrata,
-      "numero de Republicanos": republican.length,
-      "Promedio de votos Republicanos": resultadoRepublicano,
-      "numero de Independientes": independents.length,
-      "Promedio de votos Independientes": resultadoIndependiente,
-      "Votantes_menos_comprometidos": 
+      "numero_de_democratas": 0,
+      "promedio_de_votos_democratas": 0,
+      "numero_de_republicanos": 0,
+      "Promedio_de_votos_republicanos": 0,
+      "numero_de_independientes": 0,
+      "promedio_de_votos_independientes": 0,
+      "votantes_menos_comprometidos": 
         //copia de modelo de json para para almacenar dotas del array en el objeto
          {
           datos: [
-            { nombre: " ", numero_perdidas: 0, votes_with_party_pct: 0 }
+            { nombre: " ", numero_perdidas: 0, missed_votes_pct: 0 }
           ]
-        } 
-      
+        },
+        "votantes_mas_comprometidos": 
+        {
+          datos: [
+            { nombre: " ", numero_perdidas: 0, missed_votes_pct: 0 }
+          ]
+        },
+        "personas_menos_votan": 
+        {
+          datos: [
+            { nombre: " ", total_votes: 0, votes_with_party_pct: 0 }
+          ]
+        },
+        "personas_mas_votan": 
+        {
+          datos: [
+            { nombre: " ", total_votes: 0, votes_with_party_pct: 0 }
+          ]
+        }  
     };
-    //--+--+-+-+-+-+--+-+-++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-++-+-+-+-+-+-+-+-+--+-+-+-+-+-+-+-+-+-+-+-+-+-++
-    //valores del nuevo JSON
-    var obj={} ,data;
-    data =JSON.stringify(estadisticas);
-    obj= JSON.parse(data);
-  
+
+    mostrarEstadisticas(arreglo,estadisticas)
+    
+ }//fin de la instrucion para capturar el json
+
+//asigo dos parametros para que realice las estadisticas el primero es el json global y el segundo el josn stadistica
+function mostrarEstadisticas(valor1, valor2)
+    {
+      var valorDemocrata = 0,
+      valorRepublicano = 0,
+      valorIndependiente = 0;
+//+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/++/+/+/+/+/+/+/+/+/+/+/
+//parte 1
+var obj={} ,data;
+data =JSON.stringify(valor2);
+obj= JSON.parse(data);
+
+
+      democrata = valor1.filter(arre => arre.party == "D");
+      republican = valor1.filter(arre => arre.party == "R");
+      independents = valor1.filter(arre => arre.party == "I");
+      //console.log(democrata)
+    
+      for (let i = 0; i < democrata.length; i++) {
+        valorDemocrata += democrata[i].votes_with_party_pct;
+      }
+    
+      for (let i = 0; i < republican.length; i++) {
+        valorRepublicano += republican[i].votes_with_party_pct;
+      }
+    
+      for (let i = 0; i < independents.length; i++) {
+        valorIndependiente += independents[i].votes_with_party_pct;
+      }
+    
+       //--+--+-+-+-+-+--+-+-++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-++-+-+-+-+-+-+-+-+--+-+-+-+-+-+-+-+-+-+-+-+-+-++
+    //carga de la primera estadistica en el JSON stadisticas
+ 
+      resultadoDemocrata = (valorDemocrata / democrata.length).toFixed(2);
+      resultadoRepublicano = (valorRepublicano / republican.length).toFixed(2);
+      resultadoIndependiente = valorIndependiente;
+
+      obj.numero_de_democratas = democrata.length;
+      obj.promedio_de_votos_democratas = resultadoDemocrata;
+      obj.numero_de_republicanos = republican.length; 
+      obj.Promedio_de_votos_republicanos = resultadoRepublicano;
+      obj.numero_de_independientes = independents.length;
+      obj.promedio_de_votos_independientes = resultadoIndependiente;
+   
   //*/*/*/**/*/*/*/*/*/*/*/ */*/*/*//*/*/*/*/*/*/*/*/*/*/*//*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*
   //Parte 2
   
   //el metodo con un callback para  que me determine las personas del partido que menos votan
-  arreglo.sort(function(a, b) {
+  valor1.sort(function(a, b) {
     if (a.missed_votes > b.missed_votes) {
       return 1;
     }
@@ -103,22 +119,15 @@ function listarPartidos(arreglo)
     return 0;
   });
   //+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/++/+/+/+/+/+/+/+/+/+/+/
-  //creacion de la tabla 
-  //el ciclo recorre el 10% de los votantes
-  var tabla1 = document.getElementById("estadistica1")
-  var mostrar1 =""
-  mostrar1 ="<thead class='thead-dark'><tr><th>Name</th><th>Number of Missed</th><th>Votes	% Missed</th></thead>"
-  for (let i = 0; i < (arreglo.length * 10) / 100; i++) {
-    //muestra el 10% de personas que no votan por el partido
-    mostrar1 += "<tr><td>"+ arreglo[i].first_name +"</td><td>"+ arreglo[i].missed_votes +"</td><td>"+arreglo[i].missed_votes_pct+"</td></tr>"
-    
-    obj.Votantes_menos_comprometidos['datos'].push({"nombre":arreglo[i].first_name,"numero_perdidas":arreglo[i].missed_votes,"votes_with_party_pct":arreglo[i].missed_votes_pct})
-  }
-   tabla1.innerHTML=mostrar1 
+  //muestra el 10% de personas menos comprometidas del partido
+  for (let i = 0; i < (valor1.length * 10) / 100; i++) {    
+    obj.votantes_mas_comprometidos['datos'].push({"nombre":valor1[i].first_name,"numero_perdidas":valor1[i].missed_votes,"missed_votes_pct":valor1[i].missed_votes_pct})
+  }    
   //+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/+/++/+/+/+/+/+/+/+/+/+/+/
+
   //*/*/*/**/*/*/*/*/*/*/*/*/*/*/*//*/*/*/*/*/*/*/*/*/*/*//*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*
   //Parte 3
-  arreglo.sort(function(a, b) {
+  valor1.sort(function(a, b) {
     if (a.missed_votes > b.missed_votes) {
       return -1;
     }
@@ -129,24 +138,21 @@ function listarPartidos(arreglo)
     // si son iguales
     return 0;
   });
-  var tabla2 = document.getElementById("estadistica2")
-  var mostrar2 =""
-  mostrar2 ="<thead class='thead-dark'><tr><th>Name</th><th>Number of Missed</th><th>Votes	% Missed</th></thead>"
-  for (let i = 0; i < (arreglo.length * 10) / 100; i++) {
-    //muestra el 10% de personas que no votan por el partido
-    mostrar2 += "<tr><td>"+ arreglo[i].first_name +"</td><td>"+ arreglo[i].missed_votes +"</td><td>"+arreglo[i].missed_votes_pct+"</td></tr>"
-    //obj['datos'].push("nombre" : arreglo[i].first_name)
-  }
-   tabla2.innerHTML=mostrar2 
 
+
+  //el ciclo recorre el 10% de los votantes mas comprometidos
+
+  for (let i = 0; i < (valor1.length * 10) / 100; i++) {    
+    obj.votantes_menos_comprometidos['datos'].push({"nombre":valor1[i].first_name,"numero_perdidas":valor1[i].missed_votes,"missed_votes_pct":valor1[i].missed_votes_pct})
+  }
   //*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*//*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*
+
 //*/*/*/**/*/*/*/*/*/*/*/*/*/*/*//*/*/*/*/*/*/*/*/*/*/*//*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*
 //Parte 4
-arreglo.sort(function(a, b) {
+valor1.sort(function(a, b) {
   if (a.votes_with_party_pct > b.votes_with_party_pct) {
     return -1;
   }
-
   if (a.votes_with_party_pct < b.votes_with_party_pct) {
     return 1;
   }
@@ -154,40 +160,50 @@ arreglo.sort(function(a, b) {
   return 0;
 });
 
-var mostrar3 =""
-mostrar3 ="<thead class='thead-dark'><tr><th>Name</th><th>Number of Missed</th><th>Votes	% Missed</th></thead>"
-for (let i = 0; i < (arreglo.length * 10) / 100; i++) {
-  //muestra el 10% de personas que no votan por el partido
-  mostrar3 += "<tr><td>"+ arreglo[i].first_name +"</td><td>"+ arreglo[i].total_votes +"</td><td>"+arreglo[i].votes_with_party_pct+"</td></tr>"
+ //muestra el 10% de personas mas votan por el partido
+for (let i = 0; i < (valor1.length * 10) / 100; i++) {
+  obj.personas_mas_votan['datos'].push({"nombre":valor1[i].first_name,"total_votes":valor1[i].total_votes,"votes_with_party_pct":valor1[i].votes_with_party_pct})
+ 
 }
- //console.log(mostrar3) 
+//*/*/*/**/*/*/*/*/*/*/*/*/*/*/*//*/*/*/*/*/*/*/*/*/*/*//*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*
 
 //*/*/*/**/*/*/*/*/*/*/*/*/*/*/*//*/*/*/*/*/*/*/*/*/*/*//*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*
-//*/*/*/**/*/*/*/*/*/*/*/*/*/*/*//*/*/*/*/*/*/*/*/*/*/*//*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*
   //Parte 5
-  arreglo.sort(function(a, b) {
+  valor1.sort(function(a, b) {
     if (a.votes_with_party_pct > b.votes_with_party_pct) {
       return -1;
     }
-
     if (a.votes_with_party_pct < b.votes_with_party_pct) {
       return 1;
     }
     // si son iguales
     return 0;
   });
- // var tabla2 = document.getElementById("estadistica2")
-  var mostrar4 =""
-  mostrar4 ="<thead class='thead-dark'><tr><th>Name</th><th>Number of Missed</th><th>Votes	% Missed</th></thead>"
-  for (let i = 0; i < (arreglo.length * 10) / 100; i++) {
-    //muestra el 10% de personas que no votan por el partido
-    mostrar4 += "<tr><td>"+ arreglo[i].first_name +"</td><td>"+ arreglo[i].total_votes +"</td><td>"+arreglo[i].votes_with_party_pct+"</td></tr>"
+ 
+  //muestra el 10% de personas que menos  votan por el partido
+  for (let i = 0; i < (valor1.length * 10) / 100; i++) {
+    obj.personas_menos_votan['datos'].push({"nombre":valor1[i].first_name,"total_votes":valor1[i].total_votes,"votes_with_party_pct":valor1[i].votes_with_party_pct})
   }
-   //console.log(mostrar4)
-
   //*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*//*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*
+   //console.log(JSON.stringify(obj));
+    crearTablas(obj)
+  }
 
+  function crearTablas(valor1)
+  {
+    var str = valor1
 
+    var estadisticaAsistencia = document.getElementById("menos_asistencia")
+    var estadisticaPartido = document.getElementById("menos_partido")
+    var mostrarTabla2 =""
+    var mostrar = str.votantes_menos_comprometidos.datos
+    mostrarTabla2 = "<tbody>"
+    for(let i=1;i<str.votantes_menos_comprometidos.datos.length;i++)
+    {     
+      mostrarTabla2 +="<tr><td>"+mostrar[i].nombre+"</td> <td>"+ mostrar[i].numero_perdidas+"</td> <td>"+ mostrar[i].missed_votes_pct+"</td> </tr>"
+    }
+    estadisticaAsistencia.innerHTML=mostrarTabla2
+     
 
-   console.log(JSON.stringify(obj));
-}
+   //Estadistica 1
+  }
